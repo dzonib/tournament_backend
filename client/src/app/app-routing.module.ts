306d6e13fae8954b3c3097b191feb8ee7c4a3ad0
dzonib <nikolabosnjak381@gmail.com>
2019-06-27 10:15:10 +0200

    workin

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { LoginComponent } from "./components/auth/login/login.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { AuthGuard } from './data/guards/auth-guard.guard';
 import { LoginGuard } from './data/guards/login.guard';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  { path: "", component: DashboardComponent, canActivate: [AuthGuard] },
   { path: "login", component: LoginComponent, canActivate: [LoginGuard] },
-  { path: "dashboard", component: DashboardComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
